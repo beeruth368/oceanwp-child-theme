3bffa7c19a283860e5bb4de76d487988a2c8f5a5
Brian Rutherford <96803819+beeruth368@users.noreply.github.com>
2022-01-03 21:25:12 -0500

    Setup
    Setup

---
+++ functions.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Load the parent style.css file
+ *
+ * @link http://codex.wordpress.org/Child_Themes
+ */
+function oceanwp_child_enqueue_parent_style() {
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
+	$theme   = wp_get_theme( 'OceanWP' );
+	$version = $theme->get( 'Version' );
+	// Load the stylesheet
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
+	
+}
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
+?>
+<?php
+add_action( 'elementor/widget/render_content', function( $output, $widget ) {
+	//make sure its an image box widget
+	if ( 'image-box' !== $widget->get_name() ) {
+		return $output;
+	}
+	$settings = $widget->get_settings_for_display();
+	// Wrap paragraph in link
+	$output = preg_replace( '/<p[^>]+>([^<]+)<\/p>/i',
+		'<a ' . $widget->get_render_attribute_string( 'link' ) . '>\0</a>', $output );
+	return $output;
+}, 10, 2 );
+
+    // Globalize the metaboxes array, this holds all the widgets for wp-admin
+function remove_dashboard_meta() {
+    remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+}
+add_action( 'admin_init', 'remove_dashboard_meta' );
+
+function disable_elementor_dashboard_overview_widget() {
+	remove_meta_box( 'e-dashboard-overview', 'dashboard', 'normal');
+}
+add_action('wp_dashboard_setup', 'disable_elementor_dashboard_overview_widget', 40);
+add_action('admin_init', function () {
+    // Redirect any user trying to access comments page
+    global $pagenow;
+    
+    if ($pagenow === 'edit-comments.php') {
+        wp_redirect(admin_url());
+        exit;
+    }
+
+    // Remove comments metabox from dashboard
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+    // Disable support for comments and trackbacks in post types
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+});
+
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+
+// Remove comments page in menu
+add_action('admin_menu', function () {
+    remove_menu_page('edit-comments.php');
+});
+
+// Remove comments links from admin bar
+add_action('init', function () {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+});
+?>
---
+++ readme.md
@@ -0,0 +1,8 @@
+A simple, easy to use OceanWP Childtheme for WordPress
+
+A few notes:
+- Functions.php has modifications to do the following:
+   - Remove all default Dashboard widgets including Elementor Editor News (excluding Site Health)
+   - Remove Comments from WP Menu
+   - Close Previous Comments
+   - Fixes Bug in Elementor to allow the image box to be fully clickable if selected for a link
